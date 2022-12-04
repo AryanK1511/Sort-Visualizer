b9f9b72a685baf5adc4301dba9e4073f2d9f513b
Aryan Khurana <aryankhurana1511@gmail.com>
2022-12-04 18:24:24 -0500

    Created full frontend with styling

--- script.js
+++ script.js
@@ -7,56 +7,65 @@ const bubbleSortButton = document.getElementById("bubble-sort-selection");
 const selectionSortButton = document.getElementById("selection-sort-selection");
 const insertionSortButton = document.getElementById("insertion-sort-selection");
 
+let arr = [];
+
 // Function to generate a random number within the given range
-// function generateRandomNumber(start, end) {
-//     return (Math.floor(Math.random() * (end - start) + start));
-// }
+function generateRandomNumber(start, end) {
+    return (Math.floor(Math.random() * (end - start) + start));
+}
 
 // Function to create bars
-// function generateBars(numberOfBars = 25) {
-//     for (let i = 0; i < numberOfBars; i++) {
-//         const bar = document.createElement('div');
-//         const barHeight = generateRandomNumber(1, 30);
-//         bar.classList.add('bar');
-//         bar.style.height = `${barHeight * 10}px`;
-//         bar.style.width = `${barsContainer.style.width * 10}px`;
-//         bar.style.transform = `translateX(${i * 30}px)`;
-//         barsContainer.appendChild(bar);
-//     }
-// }
+function generateBars(numberOfBars = 25) {
+    for (let i = 0; i < numberOfBars; i++) {
+        const bar = document.createElement('div');
+        const barHeight = generateRandomNumber(1, 30);
+        bar.classList.add('bar');
+        bar.style.transform = `translateX(${i * 30}px)`;
+        bar.style.height = `${barHeight * 10}px`;
+        bar.id = i;
+        bar.style.width = `30px`;
+
+        // Pushing the height as the value of an element in the array
+        arr.push(bar.style.height);
+
+        // Adding the bar to the website
+        barsContainer.appendChild(bar);
+    }
+}
 
 // Function to delete all the bars
-// function clearBars() {
-//     let bars = document.querySelectorAll(".bar");
-//     bars.forEach(bar => barsContainer.removeChild(bar));
-// }
+function clearBars() {
+    let bars = document.querySelectorAll(".bar");
+    bars.forEach(bar => barsContainer.removeChild(bar));
+    arr = [];
+}
 
 // Ensuring that the number of bars change according to the user's choice
-// dropdownItems.forEach(dropdownItem => {
-//     dropdownItem.addEventListener('click', () => {
-//         const numberOfBarsRequestedByUser = dropdownItem.innerHTML;
-//         clearBars();
-//         generateBars(numberOfBarsRequestedByUser);
-//     })
-// })
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('click', () => {
+        const numberOfBarsRequestedByUser = dropdownItem.innerHTML;
+        clearBars();
+        generateBars(numberOfBarsRequestedByUser);
+    })
+})
 
 // Makes sure that the bars are reset once the randomize button is clicked
-// randomizeButton.addEventListener('click', () => {
-//     clearBars();
-//     generateBars(20);
-// });
+randomizeButton.addEventListener('click', () => {
+    clearBars();
+    generateBars(20);
+});
 
 // Generates bars by default when the page is loaded
-// generateBars();
+generateBars();
 
 // Changing theinformation paragraph content acording to the button clicked
 bubbleSortButton.addEventListener('click', () => {
     infoParagraph.innerHTML = `<p class="info-para">Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order.The pass through the list is repeated until the list is sorted. The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list. Although the algorithm is simple, it is too slow and impractical for most problems.
     <ul>
-      <li>Worst-case time complexity	O(n^2)</li>
-      <li>Average time complexity	O(n^2)</li>
-      <li>Best-case time complexity	O(n)</li>
-      <li>Worst-case space complexity	O(1)</li>
+      <li>Worst-case time complexity: O(n^2)</li>
+      <li>Average time complexity: O(n^2)</li>
+      <li>Best-case time complexity: O(n)</li>
+      <li>Worst-case space complexity: O(1)</li>
     </ul>
   </p>`;
 })
@@ -64,10 +73,10 @@ bubbleSortButton.addEventListener('click', () => {
 selectionSortButton.addEventListener('click', () => {
     infoParagraph.innerHTML = `<p class="info-para">Selection Sort is an in-place comparison sorting algorithm that divides the input list into two parts: the sublist of items already sorted, which is built up from left to right at the front (left) of the list, and the sublist of items remaining to be sorted that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right.
     <ul>
-      <li>Worst-case time complexity	O(n^2)</li>
-      <li>Average time complexity	O(n^2)</li>
-      <li>Best-case time complexity	O(n^2)</li>
-      <li>Worst-case space complexity	O(1)</li>
+      <li>Worst-case time complexity: O(n^2)</li>
+      <li>Average time complexity: O(n^2)</li>
+      <li>Best-case time complexity: O(n^2)</li>
+      <li>Worst-case space complexity: O(1)</li>
     </ul>
   </p>`;
 })
@@ -75,10 +84,10 @@ selectionSortButton.addEventListener('click', () => {
 insertionSortButton.addEventListener('click', () => {
     infoParagraph.innerHTML = `<p class="info-para">Insertion Sort is a simple sorting algorithm that iterates through an array and at each iteration it removes one element from the array, finds the location it belongs to in the sorted list and inserts it there, repeating until no elements remain in the unsorted list. It is an in-place, stable sorting algorithm that is inefficient on large input arrays but works well for data sets that are almost sorted. It is more efficient in practice compared to other quadratic sorting algorithms like bubble sort and selection sort.
     <ul>
-      <li>Worst-case time complexity	O(n^2)</li>
-      <li>Average time complexity	O(n^2)</li>
-      <li>Best-case time complexity	O(n)</li>
-      <li>Worst-case space complexity	O(1)</li>
+      <li>Worst-case time complexity: O(n^2)</li>
+      <li>Average time complexity: O(n^2)</li>
+      <li>Best-case time complexity: O(n)</li>
+      <li>Worst-case space complexity: O(1)</li>
     </ul>
   </p>`;
 })
